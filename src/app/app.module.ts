8428259e7ba8e73088217be0d77e982232d57fc4
PedroHBCruz <pedro.henriqueIFMT@hotmail.con>
2021-05-24 12:31:42 -0400

    Tratamento padrão para outros erros com Alert

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-
 import { MyApp } from './app.component';
-
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { CategoriaService } from '../services/domain/categoria.service';
 import { ErrorInterceptorProvider } from '../interceptors/error-interceptor';
 import { AuthService } from '../services/auth.service';
-import { StorageService } from '../services/storage_service';
 import { ClienteService } from '../services/domain/cliente.service';
 import { AuthInterceptorProvider } from '../interceptors/auth-interceptor';
+import { StorageService } from '../services/storage_service';
 
 @NgModule({
   declarations: [
--- src/interceptors/auth-interceptor.ts
+++ src/interceptors/auth-interceptor.ts
@@ -1,29 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from 'rxjs/Rx'; // IMPORTANTE: IMPORT ATUALIZADO
-import { StorageService } from '../services/storage_service';
 import { API_CONFIG } from '../config/api.config';
+import { StorageService } from '../services/storage_service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-constructor(public storage: StorageService){
-}
+    constructor(public storage: StorageService) {
+    }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-       
-       let localUser = this.storage.getLocalUser();
-       
-       let N = API_CONFIG.baseUrl.length;
-       let requestToApi = req.url.substring(0, N) == API_CONFIG.baseUrl;
 
-       if(localUser && requestToApi){
-        const authReq = req.clone({headers: req.headers.set('Authorization', 'Bearer ' + localUser.token)});
-        return next.handle(authReq);
-       }
-       else{
-             return next.handle(req);
-       }
+        let localUser = this.storage.getLocalUser();
+
+        let N = API_CONFIG.baseUrl.length;
+        let requestToAPI = req.url.substring(0, N) == API_CONFIG.baseUrl;
+
+        if (localUser && requestToAPI) {
+            const authReq = req.clone({headers: req.headers.set('Authorization', 'Bearer ' + localUser.token)});
+            return next.handle(authReq);
+        }
+        else {
+            return next.handle(req);
+        }
     }
 }
 
